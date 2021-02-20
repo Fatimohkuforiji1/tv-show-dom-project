185495c3daed1a909ca47abfa3270bad7196517b
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-02-20 09:22:50 +0000

    Added Level 200

--- script.js
+++ script.js
@@ -1,36 +1,105 @@
 //You can edit ALL of the code here
-let allEpisodes;
-let allFilmList;  
 
 function setup() {
   let allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-  
-    let allFilmList = allEpisodes.forEach(function(show){
-    let divEl = document.createElement("div")
-    let h5El = document.createElement("h5");
-    let imgEl = document.createElement("img");
-    let pEl = document.createElement("p");
-    h5El.innerHTML = `${show.name} - S${String(show.season).padStart(2, "0") } E${String(show.number).padStart(2, "0")}`;
-    imgEl.src = `${show.image.medium}`;
-    pEl.innerHTML = `${show.summary}`;
-    // adding classes to all elements
-    divEl.classList.add("summaryContainer")
-    divEl.appendChild(h5El);
-    divEl.appendChild(imgEl);
-    divEl.appendChild(pEl);
-    document.getElementById("root").appendChild(divEl);
+  //makePageForEpisodes(allEpisodes);
+
+    let myRootEl = document.getElementById("root");
+    let MyEpisodesDiv = document.createElement("div");
+    MyEpisodesDiv.className = "episodeDiv";
     
+    function allEpisodeList(episodeList){
+    episodeList.forEach(function(show){
+    const docFrag = new DocumentFragment(); 
+    let myNewDiv = document.createElement("div")
+    myNewDiv.id = show["name"]
+    let myHeader = document.createElement("h4");
+    let myImgEl = document.createElement("img");
+    let myPEl = document.createElement("p");
+    myHeader.innerHTML = `${show.name} - S${String(show.season).padStart(2, "0") } E${String(show.number).padStart(2, "0")}`;
+    myImgEl.src = `${show.image.medium}`;
+    myPEl.innerHTML = `${show.summary}`;
+    // adding classes to all elements
+    myNewDiv.className = "summaryContainer"
+
+    myNewDiv.appendChild(myHeader);
+    myNewDiv.appendChild(myImgEl);
+    myNewDiv.appendChild(myPEl);
+    docFrag.appendChild(myNewDiv)
+    MyEpisodesDiv.appendChild(docFrag)
+
   });
+   myRootEl.appendChild(MyEpisodesDiv);
+}
 
-  let myMatchedList = document.getElementById("matchedList");
-  function getList(allFilmList){
+allEpisodeList(allEpisodes); 
+  //level 200
+    let myMatchedList = document.getElementById("matchedList");
+    let myInputEl = document.getElementById("input");
 
-  } 
-
-  let inputEl = document.getElementById("input");
-  inputEl.addEventListener("input", function(event){
+  myInputEl.addEventListener("input", function(event){
+    MyEpisodesDiv.innerHTML = "";
     let getInputValue = event.target.value;
+    let filteredEpisodes = allEpisodes.filter(function (episodeList) {
+    if (episodeList["name"].trim().toLowerCase().includes(getInputValue) 
+      || episodeList["summary"].trim().toLowerCase().includes(getInputValue))
+     // || episodeList["summary"].trim().includes(getInputValue)) {
+        return episodeList;
+      
+    })
+  let myParagraph = document.getElementById("displayParagraph")
+      myParagraph.innerHTML = `displaying ${filteredEpisodes.length}/${allEpisodes.length}`;
+      myParagraph.textContent = `displaying ${filteredEpisodes.length}/${allEpisodes.length} episodes`;
+      console.log (myParagraph);
+
+   allEpisodeList(filteredEpisodes);
+  })
+
+  function selectFunction(allEpisodes){
+       const mySelectElem = document.createElement("select");
+       let searchedEpisode = allEpisodes.forEach(function(episodes){
+       const myOptionElem = document.createElement("option");
+       myOptionElem .textContent = `S${String(episodes["season"]).padStart(2, "0")}E${String(episodes["number"]).padStart(2, "0")} - ${episodes["name"]}`
+
+  myOptionElem .onchange = function(){
+       displayEpisodes(episodes);
+    }
+       mySelectElem.appendChild(myOptionElem );
+  })  
+  const displayInput = document.getElementById("displayInput");
+ displayInput.insertBefore(mySelectElem, displayInput.firstChild);
+
+}
+selectFunction(allEpisodes);
+
+
+}
+    
+   // 
+
+// let search = myInputEl.value.toLowerCase();
+//     console.log(search)
+    
+    // getInputValue.trim().length > 0;
+    // getInputValue.trim().toLowerCase();
+    
+    // let listRemains = allEpisodes.filter(function (listEpisode) {
+    //   if (listEpisode["name"].toLowerCase().includes(getInputValue) || listEpisode["summary"].toLowerCase().includes(listEpisode)) {
+    //    return listEpisode;
+      
+    //   }
+    // })
+    //  console.log(listRemains)
+    
+    // if(getInputValue && getInputValue.trim().length && getInputValue.toLowerCase()> 0){
+
+    // }
+    //console.log(getInputValue)
+  
+//
+
+    
+
 
 //     allEpisodes.map(function(episodes){
 //       if(episodes.name.includes(inputEl.value.toLowerCase())){
@@ -46,8 +115,8 @@ function setup() {
 
    
     
-  })
-}
+//   })
+// }
 // function clearList(){
 
 // }
@@ -121,6 +190,6 @@ window.onload = setup;
 // function makePageForEpisodes(episodeList) {
 //   const rootElem = document.getElementById("root");
 //   //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
+//}
 
-// window.onload = setup;
+//window.onload = setup;
